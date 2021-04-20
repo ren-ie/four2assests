22499043f01190d74c60bc814e2ee5bd796e493d
reniie <ren.ie@outlook.com>
2021-04-20 12:38:27 +0800

    update.

--- deploy.sh
+++ deploy.sh
@@ -15,16 +15,16 @@ echo "$INFO jsdeliver project dir: `pwd`"
 git add .
 git status | tee /tmp/git.out
 git commit -m "update."
-# 检查方式 1: 检查是否有文件提交
-if [ -n "`grep 'nothing to commit, working tree clean' /tmp/git.out`" ]; then
-    echo "$WARN nothing to update. don't refresh cdn." 
-    exit 1;
-fi
 
 echo "\n$INFO [ >> GIT PUSH ]"
 git push
 
 echo "\n$INFO [ >> PURGE JSDELIVER CDN ]"
+# 检查方式 1: 检查是否有文件提交
+if [ -n "`grep 'nothing to commit, working tree clean' /tmp/git.out`" ]; then
+    echo "$WARN nothing to update. don't refresh cdn." 
+    exit 1;
+fi
 FILES=`awk -F ': ' '{print $2}' /tmp/git.out | tr '\n' ' '`
 # 把循环体括起来，后加一个 & 符号，让系统起一个新的线程后台运行命令, wait 等待任务执行完成再继续
 # https://taoyan.netlify.app/post/2020-01-02.多线程并行计算
