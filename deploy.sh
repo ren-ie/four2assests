3db3088b788c64de705452113864ea02efc164e1
reniie <ren.ie@outlook.com>
2021-05-12 23:07:26 +0800

    update.

--- deploy.sh
+++ deploy.sh
@@ -17,15 +17,11 @@ git status | tee /tmp/git.out
 git commit -m "update."
 
 echo "\n$INFO [ >> GIT PUSH ]"
-git push
+git push -f
 
 echo "\n$INFO [ >> PURGE JSDELIVER CDN ]"
-# 检查方式 1: 检查是否有文件提交
-if [ -n "`grep 'nothing to commit, working tree clean' /tmp/git.out`" ]; then
-    echo "$WARN nothing to update. don't refresh cdn." 
-    exit 1;
-fi
-FILES=`awk -F ': ' '{print $2}' /tmp/git.out | tr '\n' ' '`
+GIT_LOG=`git log -z --name-only -1 | tr '\000' ' '`
+FILES=`echo $GIT_LOG | tail -1f`
 # 把循环体括起来，后加一个 & 符号，让系统起一个新的线程后台运行命令, wait 等待任务执行完成再继续
 # https://taoyan.netlify.app/post/2020-01-02.多线程并行计算
 for ITEM in `echo ${FILES//  /}`; do {
